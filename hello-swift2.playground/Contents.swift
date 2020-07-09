8361747aad6d8a5cab1baa8782ace6fefc698e6c
Henry Phu-Henh Lam <50170584+henrymmmm@users.noreply.github.com>
2020-07-09 13:54:31 +0100

    Add files via upload

---
+++ hello-swift2.playground/Contents.swift
@@ -0,0 +1,49 @@
+// youtube: Devslopes
+// https://www.youtube.com/watch?v=mhE-Mp07RTo
+
+// 00:33:10 - Swift Booleans & Conditional Logic
+
+import UIKit
+
+var adult = false
+
+if adult == true {
+    print("You can vote.")
+} else {
+    print("Can't vote yet.")
+}
+
+
+//
+
+
+var score: Int = 85      // out of 100.
+
+if score >= 80 {
+    print("Your grade is A")
+} else if score >= 60 {
+    print("Your grade is B")
+} else if score >= 40 {
+    print("Your grade is C")
+} else {
+    print("Your grade is D")
+}
+
+
+//
+
+
+var book1 = "Hello World"
+var book2 = "Hello WORLD"
+
+var check = (book1 == book2)
+print(check)
+
+
+//
+
+var yourPassword = "Happy Bunny"
+print("Your password is \(yourPassword.count) characters long.")
+
+
+// end of 54 minutes.
